c0f2c81cd98de53fe938836c4eb3e68ad2e40390
Andr3s593 <rac.piedra@yavirac.edu.ec>
2023-01-31 20:29:13 -0500

    Implementando Server¨

---
+++ src/modules/venta.module.ts
@@ -0,0 +1,17 @@
+import { Global, Module } from '@nestjs/common';
+@Global()
+@Module({
+    imports: [DatabaseModule],
+    controllers: [
+        ProductController,
+        CategoryController
+    ],
+    providers: [
+        ...ventaProviders,
+        ProductService,
+        CategoryService,
+        
+    ],
+    exports:[],
+})
+export class VentaModule {}
